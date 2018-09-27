89cc47f830d78613b38de908daed70fd5c03b5cd
Abhijith C S <abhijithcs1993@gmail.com>
2018-09-27 22:32:20 +0530

    KOT Relay - fix

--- main.js
+++ main.js
@@ -67,7 +67,7 @@ function createWindow () {
 
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  //mainWindow.webContents.openDevTools()
 
   //Full Screen
   mainWindow.setFullScreen(true)
