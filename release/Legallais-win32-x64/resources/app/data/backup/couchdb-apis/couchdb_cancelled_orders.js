8cb76ec31c7a19abb75b2e64d4511b0d6acfef4b
Abhijith C S <abhijithcs1993@gmail.com>
2018-10-25 15:02:28 +0530

    Token Number added to Takeaway orders

--- release/Legallais-win32-x64/resources/app/data/backup/couchdb-apis/couchdb_cancelled_orders.js
+++
@@ -1,9 +0,0 @@
-{
-  "_id": "_design/orders",
-  "views": {
-    "all": {
-      "map": "function (doc) {\n  emit(doc.KOTNumber, doc);\n}"
-    }
-  },
-  "language": "javascript"
-}
--- release/Legallais-win32-x64/resources/app/data/backup/couchdb-apis/couchdb_users_names.js
+++
@@ -1,9 +0,0 @@
-{
-  "_id": "_design/names",
-  "views": {
-    "names": {
-      "map": "function(doc) {\n    if (doc.name) {\n        emit(doc.name, doc);\n    }\n}"
-    }
-  },
-  "language": "javascript"
-}
