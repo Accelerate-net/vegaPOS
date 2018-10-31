72d994350c46ff77d46bc486cb9ea9d14d749585
Abhijith C S <abhijithcs1993@gmail.com>
2018-10-31 17:16:20 +0530

    Starting Script - added

---
+++ data/backup/db-porting-scripts/start_billing.sh
@@ -0,0 +1,25 @@
+echo 
+echo 
+echo - - - - - - - - - - - - - - - - - - - - - - -
+echo Starting Zaitoon Billing Software Ver 0.0.9
+echo - - - - - - - - - - - - - - - - - - - - - - - 
+echo
+cd Desktop
+cd vegaPOS
+npm start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
