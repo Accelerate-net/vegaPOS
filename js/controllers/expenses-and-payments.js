bdb8e48b31401d61e9270a7d72c8605f6631035a
Abhijith C S <abhijithcs1993@gmail.com>
2019-04-09 23:39:29 +0530

    Expenses add button

--- js/controllers/expenses-and-payments.js
+++ js/controllers/expenses-and-payments.js
@@ -157,6 +157,28 @@ function loadAllAddedExpenses(optionalSource, optionalAnimationFlag){
 		});	
 
 	}
+
+
+
+
+
+				//Floating Button Animation
+				var $floaty = $('.floaty-alternative');
+
+				$floaty.on('mouseover click', function(e) {
+				  $floaty.addClass('is-active');
+				  e.stopPropagation();
+				});
+
+				$floaty.on('mouseout', function() {
+				  $floaty.removeClass('is-active');
+				});
+
+				$('.container').on('click', function() {
+				  $floaty.removeClass('is-active');
+				});
+
+
 }
 
 function getTypeColor(type){
