567b472e370a07b7c6fc1a6c6ff1842812460b47
Firoz-PS <ce19b048@smail.iitm.ac.in>
2021-06-01 12:03:25 +0530

    key in kot fetchall changed

--- tools/couchdb-apis/couchdb_kot_fetch.js
+++ tools/couchdb-apis/couchdb_kot_fetch.js
@@ -5,7 +5,7 @@
       "map": "function (doc) {\n  emit([doc.orderDetails.modeType], doc);\n}"
     },
     "fetchall": {
-      "map": "function (doc) {\n  if(doc.KOTNumber && doc.KOTNumber != '')\n    emit(doc.KOTNumber, doc);\n}"
+      "map": "function (doc) {\n  if(doc.KOTNumber && doc.KOTNumber != '')\n    emit(doc.table, doc);\n}"
     },
     "fetchbytable": {
       "map": "function (doc) {\n  if(doc.orderDetails.modeType == \"DINE\")\n    emit([doc.table], doc);\n}"
